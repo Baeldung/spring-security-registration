c2518455ff351f9a81a6aed72c86c7f82000f779
sachin <sachin.sachdeva@gmail.com>
2018-03-22 23:49:36 -0400

    added redirect assertion to the integration test

--- src/test/java/org/baeldung/test/UserLoginIntegrationTest.java
+++ src/test/java/org/baeldung/test/UserLoginIntegrationTest.java
@@ -1,13 +1,13 @@
 package org.baeldung.test;
 
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
 import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
 
 import javax.servlet.Filter;
 
 import org.baeldung.Application;
-import org.baeldung.spring.TestDbConfig;
 import org.baeldung.spring.TestIntegrationConfig;
 import org.junit.Before;
 import org.junit.Test;
@@ -42,13 +42,13 @@ public class UserLoginIntegrationTest {
 	
 	@Test
     public void testManageUserLoginSuccess() throws Exception  {
-		ResultActions act = mockMvc.perform(formLogin("/login").user("manager@test.com").password("testm"));
-		act.andExpect(authenticated());	
+		ResultActions resultActions = mockMvc.perform(formLogin("/login").user("manager@test.com").password("testm"));
+		resultActions.andExpect(authenticated()).andExpect(redirectedUrl("/manage.html"));
     }
 	
 	@Test
     public void testManageUserLoginWrongPasswordError() throws Exception  {
-		ResultActions act = mockMvc.perform(formLogin("/login").user("manager@test.com").password("testabc"));
-		act.andExpect(unauthenticated());	
+		ResultActions resultActions = mockMvc.perform(formLogin("/login").user("manager@test.com").password("testabc"));
+		resultActions.andExpect(unauthenticated()).andExpect(redirectedUrl("/login?error=true"));
     }
 }
