51c99366c5f1e9a1bf8114521d18b062dc773a62
Necker <wojtek.drzewicki@gmail.com>
2020-01-05 14:46:47 +0100

    task4 - adding context starting test

---
+++ src/test/java/org/baeldung/test/ApplicationContextTest.java
@@ -0,0 +1,16 @@
+package org.baeldung.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class ApplicationContextTest {
+
+    @Test
+    public void shouldTestContext() {
+
+    }
+}
