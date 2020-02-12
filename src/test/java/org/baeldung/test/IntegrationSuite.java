d967d0a8aba4cfc1672eb348534e7a60f650e613
Ijiekhuamen Rex <dev.rexijie@gmail.com>
2020-02-12 23:40:09 +0100

    Added context loads test

--- src/test/java/org/baeldung/test/IntegrationSuite.java
+++ src/test/java/org/baeldung/test/IntegrationSuite.java
@@ -4,7 +4,8 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ // @formatter:off 
+@Suite.SuiteClasses({ // @formatter:off
+    ApplicationTest.class,
     ChangePasswordIntegrationTest.class, 
     TokenExpirationIntegrationTest.class,
     RegistrationControllerIntegrationTest.class,
