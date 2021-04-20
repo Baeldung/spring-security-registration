4be91ce3cd2d328a4e384cb79d6df60e56277c53
mkodavati <murali_kodavati@intuit.com>
2021-04-20 18:34:08 +0530

    Manager changes

--- src/main/java/com/baeldung/security/MySimpleUrlAuthenticationSuccessHandler.java
+++ src/main/java/com/baeldung/security/MySimpleUrlAuthenticationSuccessHandler.java
@@ -82,10 +82,15 @@ public class MySimpleUrlAuthenticationSuccessHandler implements AuthenticationSu
     protected String determineTargetUrl(final Authentication authentication) {
         boolean isUser = false;
         boolean isAdmin = false;
+        boolean isManager = false;
         final Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
         for (final GrantedAuthority grantedAuthority : authorities) {
             if (grantedAuthority.getAuthority().equals("READ_PRIVILEGE")) {
                 isUser = true;
+            } else if (grantedAuthority.getAuthority().equals("UPDATE_PRIVILEGE")) {
+                isManager = true;
+                isUser = false;
+                break;
             } else if (grantedAuthority.getAuthority().equals("WRITE_PRIVILEGE")) {
                 isAdmin = true;
                 isUser = false;
@@ -104,6 +109,8 @@ public class MySimpleUrlAuthenticationSuccessHandler implements AuthenticationSu
             return "/homepage.html?user="+username;
         } else if (isAdmin) {
             return "/console";
+        } else if (isManager) {
+            return "/manager";
         } else {
             throw new IllegalStateException();
         }
